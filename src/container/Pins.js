18297c606f7d3548834801fa7bcf315c85b5458a
ridhogenzo <genzocuy123@gmail.com>
2022-01-04 20:23:41 +0700

    first commit

---
+++ src/container/Pins.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
+
+const Pins = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  return <div>Pins</div>;
+};
+
+export default Pins;
